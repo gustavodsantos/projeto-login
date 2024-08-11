96ac370b567f2b90e92dfd582d05d3fadcb04c4b
Gustavo Junior dos Santos <gustavojuniordos@hotmail.com>
2024-08-10 23:30:06 -0300

    criado templates restantes

--- mysite/base/urls.py
+++ mysite/base/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
 from mysite.base import views
 
 app_name = 'base'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('esqueci/', views.esqueci, name='esqueci'),
+    path('senha_enviada/', views.senha_enviada, name='senha_enviada'),
 ]
--- mysite/base/views.py
+++ mysite/base/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render, redirect
 
-from mysite.base.forms import LoginForm
+from mysite.base.forms import LoginForm, EsqueciSenha
 
 
 def home(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if not form.is_valid():
-            print("Form is not valid:", form.errors)
             return render(request, 'base/index.html', {'form': form})
-        print("Form is valid, redirecting...")
-        return redirect('base/origin.html')
+        form.save()
+        return redirect('base:login')
     form = LoginForm()
     return render(request, 'base/index.html', {'form': form})
+
+
+def login(request):
+    form = LoginForm()
+    return render(request, 'base/login.html', {'form': form})
+
+
+def esqueci(request):
+    if request.method == 'POST':
+        form = EsqueciSenha(request.POST)
+        if not form.is_valid():
+            return render(request, 'base/esqueci.html', {'form': form})
+        return redirect('base:senha_enviada')
+    form = EsqueciSenha()
+    return render(request, 'base/esqueci.html', {'form': form})
+
+
+def senha_enviada(request):
+    form = LoginForm()
+    return render(request, 'base/senha_enviada.html', {'form': form})
